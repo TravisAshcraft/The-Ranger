4834df5055e6a55c4d39a1cc7aa14561319609ce
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2019-12-27 05:47:22 -0600

    added moveable player with shooting sprite/ added moving enemy with damage dector
    this is 1/4 way done no bugs so far

---
+++ The Ranger/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    WaveConfig waveConfig;
+    List<Transform> wayPoints;
+   
+
+    int waypointIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wayPoints = waveConfig.GetWaypoints();
+        transform.position = wayPoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+       
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig;
+    }
+
+    private void Move()
+    {
+        //tells the enemy where to go when game is started and how fast to move
+        if (waypointIndex <= wayPoints.Count - 1)
+        {
+            var targetPostition = wayPoints[waypointIndex].transform.position;
+            var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position,
+                targetPostition, movementThisFrame);
+
+            //tells the enemy when target waypoint is reached moved to the next waypoint. 
+            if (transform.position == targetPostition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
